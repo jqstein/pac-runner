aa31c2385efa21a3506ce0ddfba4e37a14cfc8b3
Vincius Schmidt <vinciusjschmidt@gmail.com>
2018-05-25 10:20:14 -0300

    feat(tutorial completo até o vídeo 3)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 lastPlayerPosition;
+	private float moveDistance;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		lastPlayerPosition = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDistance = player.transform.position.x - lastPlayerPosition.x;
+
+		transform.position = new Vector3 (transform.position.x + moveDistance, transform.position.y, transform.position.z);
+
+		lastPlayerPosition = player.transform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float jump;
+	public bool isInGround;
+	public LayerMask groundLayer;
+
+	private Rigidbody2D playerRigidBody;
+	private Collider2D playerCollider;
+	private Animator playerAnimator;
+
+	// Use this for initialization
+	void Start () {
+		playerRigidBody = GetComponent<Rigidbody2D> ();
+		playerCollider = GetComponent<Collider2D> ();
+		playerAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isInGround = Physics2D.IsTouchingLayers (playerCollider, groundLayer);
+		playerRigidBody.velocity = new Vector2(speed, playerRigidBody.velocity.y);
+
+		if (isInGround && Input.GetKeyDown(KeyCode.Space)) {
+			playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jump);
+		}
+
+		playerAnimator.SetFloat ("speed", playerRigidBody.velocity.x);
+		playerAnimator.SetBool ("isInGround", isInGround);
+	}
+}
